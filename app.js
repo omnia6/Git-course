b3c5c89a1af5a57a36ce26b4329931f1480de807
omnia6 <92270183+omnia6@users.noreply.github.com>
2021-10-23 23:50:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 
+//document.querySelectorAll('a[href ^="#"]').forEach(anchor => {
+//    anchor.addEventListener("click", function (e) {
+//        e.preventDefault();
+//        document.querySelector(this.getAttribute("href")).scrollIntoView({
+//            behavior: "smooth"
+//        });
+//    });
+//});
+
+//I'm painfully tried to do this project by perfecct way but I can't because of time.I'll isa tried to accomplish it at Editing phase.
